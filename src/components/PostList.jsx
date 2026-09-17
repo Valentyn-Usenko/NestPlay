45d5f569f61872dcdf34d5415f3a5a15c857aa36
Valentyn-Usenko <valentyn.usenko@gmail.com>
2026-09-17 01:22:53 -0400

    Fix PostList encoding

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,5 @@
 import PinnedBadges from './PinnedBadges'
-﻿import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   getFriendCount,
   getPosts,
